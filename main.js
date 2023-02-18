4830f5c5d14483c9296f6d3fa508d8b125ae81de
Exiledspoon <91276251+Exiledspoon@users.noreply.github.com>
2023-02-18 12:56:58 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,22 @@
+ThunkableWebviewerExtension.postMessage(rdmnmbr)
+
+function Checkforcode() {
+
+  userincode = document.getElementById('code').value
+  if (userincode == rdmnmbr) {
+    window.open("videocll.html");
+    document.getElementById('result').innerHTML = 'Logging you in'
+  }
+  else
+    document.getElementById('result').innerHTML = 'Code Invalid'
+
+
+}
+function checkifverified() {
+
+
+  if (verified == "false") {
+
+    window.open("index.html", "_self");
+  }
+}
